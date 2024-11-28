8f5574cbb83f2c4c7d190815d845ced409f4fb12
MicheleSaraceni <michele.saraceni23@email.com>
2024-11-28 17:23:14 +0100

    point 2

--- index.js
+++ index.js
@@ -6,3 +6,6 @@
 4. Impostiamo un nuovo script “watch” in package.json che possa essere lanciato con npm run dev e che aggiorni in tempo reale le modifiche ai nostri file. Lanciamolo e proviamo a cambiare il nostro codice in modo che stampi nel terminale “Hello Boolean”. Dovremmo vedere il terminale senza fermare e rilanciare il server.
 5. Proviamo a passare da terminale una parola come argomento. da mostrare dopo “Hello”. Dovremo quindi stampare “Hello parola_passata”
 */
+
+console.log("Hello World!")
+console.log(process.argv);
